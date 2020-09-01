177f210ea9b947039f8ecf1a9c365ccc716de8a9
chrishabach <christian.habach@gmail.com>
2020-09-01 19:58:28 -0400

    UPD

--- src/app/components/tarjeta/tarjeta.component.ts
+++ src/app/components/tarjeta/tarjeta.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CheckoutService } from '../../services/checkout.service';
 import { ApiMercadopagoService } from '../../services/api-mercadopago.service';
 import { PostgresService } from 'src/app/services/postgres/postgres.service';
+import { MatTableDataSource, MatSort, MatPaginator} from '@angular/material';
+import { Subscription } from 'rxjs';
 import { get } from 'scriptjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { getHeaderStts } from '../utility';
 
 @Component({
   selector: 'app-tarjeta',
@@ -10,10 +14,15 @@ import { get } from 'scriptjs';
   styleUrls: ['./tarjeta.component.css']
 })
 export class TarjetaComponent implements OnInit {
+  displayedColumns: string[] = ['fecha', 'id', 'campana', 'bco_origen','bco_destino', 'tools'];
+  dataSource: MatTableDataSource<any> = new MatTableDataSource();
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
   hoy : any;
   init_point: any;
-  
- 
+  headers = null;
+  user=null;
+  getLeadByBancoSubscription: Subscription;
     items={
         
             name: 'Mi producto',
@@ -22,9 +31,29 @@ export class TarjetaComponent implements OnInit {
         }
     
   
-  constructor( private postgresqlService: PostgresService,private checkoutService: CheckoutService,private apiMercadopagoService: ApiMercadopagoService) { }
+  constructor( private postgresqlService: PostgresService,
+    private checkoutService: CheckoutService,
+    private apiMercadopagoService: ApiMercadopagoService,
+    private authService: AuthService) { 
+
+    }
 
   ngOnInit(): void {
+    
+    this.authService.getestado();
+    this.headers = getHeaderStts(this.authService.isUserLoggedIn())
+
+
+    this.user =this.authService.isUserLoggedIn();
+    //this.user.email  julio.arellano@cajalosandes.cl
+    this.postgresqlService.getUsuarioPorMail(this.user.email, getHeaderStts(this.user)).subscribe(
+      resp => {
+        if(resp){
+          this.user = resp[0];
+          console.log(this.user);
+          this.getLeadByBanco();
+        }
+      });
   /*  this.postgresqlService.getFlow(this.items).subscribe(res=>{
       
         console.log(res);
@@ -57,6 +86,24 @@ export class TarjetaComponent implements OnInit {
         //window.location.href = this.init_point;
       });*/
   }
+
+  getLeadByBanco(){
+      this.getLeadByBancoSubscription = this.postgresqlService.getLeadByBanco(this.user.idbanco, this.headers).subscribe((data: any) => {
+        console.log(data);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.data = data;
+      });
+  }
+  
+  
+
+  applyFilter(value: string) {
+    let filterValue = value.trim();
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+    }
+
 }
 
 
--- src/app/services/postgres/postgres.service.ts
+++ src/app/services/postgres/postgres.service.ts
@@ -677,6 +677,11 @@ export class PostgresService {
           retry(1),catchError(this.errorHandl))
       }
 
+      getLeadByBanco(idbanco: any, headers): Observable<any>{
+        let complete_uri = `${this.BASE_URI}/cambiate_leads/banco?idbanco=${idbanco}`;
+        return this.httpGet(complete_uri, headers);
+      }
+
   errorHandl(error: any) {
     let errorMessage = '';
     if(error.error instanceof ErrorEvent) {
